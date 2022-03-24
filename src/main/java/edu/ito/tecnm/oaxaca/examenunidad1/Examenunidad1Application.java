80eefb0b97fd233bfa8d4140efbdf37a38386498
Ariel-Canseco <18161156@itoaxaca.edu.mx>
2022-03-23 19:22:08 -0600

    Anexando el proyecto

---
+++ src/main/java/edu/ito/tecnm/oaxaca/examenunidad1/Examenunidad1Application.java
@@ -0,0 +1,13 @@
+package edu.ito.tecnm.oaxaca.examenunidad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Examenunidad1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Examenunidad1Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/ito/tecnm/oaxaca/examenunidad1/Examenunidad1ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.ito.tecnm.oaxaca.examenunidad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Examenunidad1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
